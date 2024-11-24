10aa70dc2d98f62c8e07f49504bbb82e8287099c
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-24 20:59:09 -0300

    feat: customize header

--- AluraViagens/ViewController.swift
+++ AluraViagens/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         viagensTableView.dataSource = self
         viagensTableView.delegate = self
+        
+        view.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
     }
 }
 
