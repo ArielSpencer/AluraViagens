c30d722fc2eb628f71e3ceac6ab05997849ebf2e
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-24 21:29:24 -0300

    chore: configure constraints

--- AluraViagens/HomeTableViewHeader.swift
+++ AluraViagens/HomeTableViewHeader.swift
@@ -12,9 +12,10 @@ class HomeTableViewHeader: UIView {
     // MARK: - IBOutlets
     
     @IBOutlet weak var tituloLabel: UILabel!
-    
     @IBOutlet weak var headerView: UIView!
-    
     @IBOutlet weak var bannerImageView: UIImageView!
     
+    func configuraView() {
+        headerView.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
+    }
 }
