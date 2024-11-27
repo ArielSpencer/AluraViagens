4bd38131c7fcf235c2aed28b887f049b49cbaa97
Ariel Spencer <arielspencer.tech@gmail.com>
2024-11-26 22:17:00 -0300

    style: round view corners and adjust header constraints

--- AluraViagens/View/Header/HomeTableViewHeader.swift
+++ AluraViagens/View/Header/HomeTableViewHeader.swift
@@ -14,8 +14,16 @@ class HomeTableViewHeader: UIView {
     @IBOutlet weak var tituloLabel: UILabel!
     @IBOutlet weak var headerView: UIView!
     @IBOutlet weak var bannerImageView: UIImageView!
+    @IBOutlet weak var bannerView: UIView!
+    
     
     func configuraView() {
         headerView.backgroundColor = UIColor(red: 30/255, green: 59/255, blue: 119/255, alpha: 1)
+        
+        bannerView.layer.cornerRadius = 10
+        bannerView.layer.masksToBounds = true
+        
+        headerView.layer.cornerRadius = 500
+        headerView.layer.maskedCorners = [.layerMinXMaxYCorner, .layerMaxXMaxYCorner]
     }
 }
